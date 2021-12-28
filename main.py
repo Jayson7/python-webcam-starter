ef020fd34226ef60f1a3c8cade331b322e011d7c
jayson7 <lexxiijoo70@gmail.com>
2021-12-28 12:43:45 +0100

    webcam starter

--- main.py
+++ main.py
@@ -6,4 +6,5 @@ if  capt.isOpened()  == False:
     raise IOError("Webcam can't open")
 while True:
     ret, frame = capt.read()
+    frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_AREA)
     
