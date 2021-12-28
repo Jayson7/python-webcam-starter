1dc7dc989eb84dd0a008f1d6c277e80bed0ec425
jayson7 <lexxiijoo70@gmail.com>
2021-12-28 12:52:38 +0100

    webcam starter

--- main.py
+++ main.py
@@ -9,3 +9,5 @@ while True:
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation = cv2.INTER_AREA)
     cv2.imshow("Input", frame)
     c = cv2.waitKey(1)
+    if c == 27:
+        break 
